efdfbd5dfcf475df4928ac489952dd0495b96f57
Neta Ronen <netaronen29@gmail.com>
2025-09-13 19:33:29 +0300

    Initial commit

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,43 @@
+import { Component, inject, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  private httpClient = inject(HttpClient);
+
+
+  ngOnInit() {
+    this.isFetching.set(true);
+    this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places')
+    .pipe(
+      map((resData) => resData.places)
+     )
+    .subscribe({
+      next : (places) => {
+       this.places.set(places);
+    },
+    complete : () => {
+    this.isFetching.set(false);
+    }
+  });
+
+
+  
+  }
+
+
+
+}
